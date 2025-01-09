856911e0520d6562a112ee469a97d17bef9b1759
Fluxx7 <nwa31104@gmail.com>
2025-01-08 21:39:32 -0500

    changed config to not be static

--- next_source/next.config.ts
+++ next_source/next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // basePath: "/{github pages base path}}",
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
 };
 
 export default nextConfig;
